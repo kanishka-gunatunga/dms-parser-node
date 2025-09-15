0cd1a86a1d7c58621eb781123b9edbbf4ead5826
madushan10 <mnmnjith@gmail.com>
2025-09-15 09:39:54 +0530

    update 2

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 
 const express = require('express');
 const extractTextRoute = require('./api/extract-text');
-const anotherRoute = require('./api/another-route'); 
+const extractTextPagesRoute = require('./api/extract-text-pages'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post('/api/extract-text', extractTextRoute);
-app.post('/api/extract-text-pages', anotherRoute); 
+app.post('/api/extract-text-pages', extractTextPagesRoute); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
